e97abcb110b96745151f3d962574eca75ef17e3d
Suguru <suu.rockfield@gmail.com>
2021-06-13 21:17:09 +0900

    テストコードを修正

--- tests/test_storage_access.py
+++ tests/test_storage_access.py
@@ -3,7 +3,7 @@ import pytest
 from fudowatch.storage_access import FiresoreCrient
 
 
-class TestObject():
+class SampleObject():
     def __init__(self, name='name', age=1, height=1.1, adult=False, other=[1]):
         self.name = name
         self.age = age
@@ -13,8 +13,8 @@ class TestObject():
 
 
 def test_storage_access():
-    test_obj_list = [TestObject(),
-                     TestObject('name2', 2, 2.2, True, [1, 2])]
+    test_obj_list = [SampleObject(),
+                     SampleObject('name2', 2, 2.2, True, [1, 2])]
 
     # 例外が起こらないことを確認
     try:
